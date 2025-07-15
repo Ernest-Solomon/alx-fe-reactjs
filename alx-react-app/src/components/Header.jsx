bb9f99f1babaf641f977cec5552532fe86ee22ed
Ernest Solomon <marveldad2@gmail.com>
2025-07-15 14:08:52 +0100

    make correction o tast2

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,16 @@
+    // src/Header.jsx
+
+    // Define a functional component named Header
+    function Header() {
+      return (
+        // The header element for the application
+        <header>
+          {/* Main heading for the page */}
+          <h1>My Favorite Cities</h1>
+        </header>
+      );
+    }
+
+    // Export the Header component so it can be used in other files
+    export default Header;
+    
