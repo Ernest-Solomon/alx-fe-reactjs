86449807d9f97450ab1f493e90bfc911e79ccb9d
Ernest Solomon <marveldad2@gmail.com>
2025-08-10 17:13:05 +0100

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,15 @@
 // src/App.jsx
 import React from 'react';
-import UserSearch from './components/UserSearch';
-import './App.css'; // Make sure to import your CSS file
+// Change this line:
+// import UserSearch from './components/UserSearch';
+// To this:
+import Search from './components/Search';
+import './App.css'; 
 
 function App() {
   return (
     <div className="bg-gray-100 min-h-screen">
-      <UserSearch />
+      <Search />
     </div>
   );
 }
